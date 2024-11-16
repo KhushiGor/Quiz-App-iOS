c6da8e5e25331436eb13cfbc7b13abaedc340518
KhushiGor <khushigorpersonal@gmail.com>
2024-11-16 18:06:34 -0500

    shared delegate

---
+++ Quiz-App/QuestionCellUpdateTableViewCell.swift
@@ -0,0 +1,23 @@
+//
+//  QuestionCellUpdateTableViewCell.swift
+//  Quiz-App
+//
+//  Created by Khushi Mineshkumar Gor on 2024-11-16.
+//
+
+import UIKit
+
+class QuestionCellUpdateTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
